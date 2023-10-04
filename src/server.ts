86e764fb115cf9f191c025d9e99bb0101fda7ca7
Naimur <naimur.r61n@gmail.com>
2023-10-04 17:10:29 +0600

    Problem fixed

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import mongoose from "mongoose";
+import express from "express";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-   res.send('Hello World!')
-})
+const app = express();
+const port = 7000;
+app.use(cors());
 
+// Databases connection
+async function bootstrap() {
+	await mongoose.connect("mongodb://127.0.0.1:27017/test");
+	try {
+		console.log("Database connection Done!");
 
-app.listen(port, () => {
-   console.log(`Example app listening on port ${port}`)
-})
+		// Listing databases
+		app.listen(port, () => {
+			console.log(`Server is listening on port ${port}`);
+		});
+	} catch (error) {
+		console.log("Fail to connect database.", error);
+	}
+}
+
+bootstrap();
+
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
